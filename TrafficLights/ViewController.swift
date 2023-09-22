59d5a91220304cef0dae9923310b7f8c1e37911d
IrinaMuravyova <miomir@yandex.ru>
2023-09-22 12:10:34 +0800

    Добавлены связи элементов Storeryboard  с кодом

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redView: UIView!
+    
+    @IBOutlet var yellowView: UIView!
+    
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var trafficLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func trafficLightButtonSwitch() {
+    }
+    
 }
 
